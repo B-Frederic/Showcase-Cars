c50fe7b721be881a63e4d4f23526714d6d1c727e
B-Frederic <87258430+B-Frederic@users.noreply.github.com>
2023-06-15 11:26:51 +0200

    add: api car + card

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,57 @@
 import Image from "next/image";
 // Components
 import Hero from '@/components/Hero'
-import Catalogue from "@/components/Catalogue";
+import SearchBar from "@/components/SearchBar";
+import Filter from "@/components/Filter";
+import CarCard from "@/components/CarCard";
+// Utils
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+
+  const allCars = await fetchCars();    
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
-      <Catalogue />
+      
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+            <h2 className="text-4xl font-extrabold">Catalogue voitures</h2>
+            <p>Trouver la voiture de vos rêves sans plus attendre</p>
+        </div>
+
+        <div className="home__filters">
+            <SearchBar />
+            <div className="home__filter-container">
+                <Filter title="fuel" />
+                <Filter title="year" />
+            </div>
+        </div>
+      </div>
+
+      {!isDataEmpty ? (
+        <section>
+          <div className='home__cars-wrapper'>
+            {allCars?.map((car) => (
+              <CarCard car={car} />
+            ))}
+          </div>
+
+          {/* <ShowMore
+            pageNumber={(searchParams.limit || 10) / 10}
+            isNext={(searchParams.limit || 10) > allCars.length}
+          /> */}
+        </section>
+      ) : (
+        <div className='home__error-container'>
+          <h2 className='text-black text-xl font-bold'>Désolé, aucun résultat</h2>
+          <p>{allCars?.message}</p>
+        </div>
+      )}
+
     </main>
   )
 }
--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,39 @@
 import { MouseEventHandler } from "react";
 
 export interface ButtonProps {
-    title: string;
+    isDisabled?: boolean;
+    btnType?: "button" | "submit";
     containerStyles?: string;
+    textStyles?: string;
+    title: string;
+    rightIcon?: string;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export interface SearchManufacturerProps {
     manufacturer: string,
     setManufacturer: (manufacturer: string) => void;
+}
+
+export interface FilterProps {
+    manufacturer?: string;
+    year?: number;
+    model?: string;
+    limit?: number;
+    fuel?: string;
+}
+
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
 }
